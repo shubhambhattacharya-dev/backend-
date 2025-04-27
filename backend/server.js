79a6007085060a213f6b6ae08a7d6ce4c8754648
shubhambhattacharya-dev <get.input.2025@gmail.com>
2025-04-27 22:19:17 +0530

    nodemon

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,6 @@ import express from 'express';
 const app=express();
 
 app.listen(5000,()=>{
-
-    console.log("server started at https://localhost:5000");
+    console.log("server started at https://localhost:5000 bhhu");
 
 })
